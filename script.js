f9ca6efe3d97c53d2e7279f2e7db053883d62629
Aradhya <26aradhyajha@gmail.com>
2026-07-01 16:53:13 +0530

    Setup JavaScript selectors for form elements and modify html

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("signup-form");
+
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passInput = document.getElementById("password");
+const confirmPassInput = document.getElementById("confirm-password");
+
+const nameError = document.getElementById("name-error");
+const emailError = document.getElementById("email-error");
+const passError = document.getElementById("password-error");
+const confirmPassError = document.getElementById("confirm-password-error");
